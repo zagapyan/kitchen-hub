6e8ef9c9fe6300b901bf3989bbf86524b2c063c2
Zigmund Sun Oo <zigmundsunoo@gmail.com>
2018-04-03 20:19:23 -0500

    [hotfix] pointer events deleted

--- src/components/PaginationComponent/PaginationComponent.js
+++ src/components/PaginationComponent/PaginationComponent.js
@@ -1,14 +1,21 @@
 import React from "react";
-import { Link } from 'react-router-dom'
+import { Link } from "react-router-dom";
 import PropTypes from "prop-types";
 import styles from "./PaginationComponent.scss";
 
 const PaginationComponent = ({ props }) => (
   <nav className="pagination" role="navigation" aria-label="pagination">
-    {props.currentPage > 1 ? (<Link className="pagination-previous" to={`/page/${parseInt(props.currentPage)-1}`}>
+    <Link
+      className="pagination-previous"
+      to={`/page/${parseInt(props.currentPage) - 1}`}
+      disabled={props.currentPage <= 1}
+    >
       Previous
-    </Link>) : ''}
-    <Link className="pagination-next" to={`/page/${parseInt(props.currentPage)+1}`}>
+    </Link>
+    <Link
+      className="pagination-next"
+      to={`/page/${parseInt(props.currentPage) + 1}`}
+    >
       Next page
     </Link>
   </nav>
